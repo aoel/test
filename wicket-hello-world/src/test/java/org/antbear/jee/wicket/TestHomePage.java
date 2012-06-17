cc0420a17e0a45f690055266a37b361f010c6769
aoel <armin.oellinger@ib-oellinger.de>
2012-06-17 20:57:44 +0200

    wicket-hello-world

---
+++ wicket-hello-world/src/test/java/org/antbear/jee/wicket/TestHomePage.java
@@ -0,0 +1,32 @@
+package org.antbear.jee.wicket;
+
+import org.antbear.jee.wicket.WicketApplication;
+import org.antbear.jee.wicket.HelloWorld;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage {
+
+    private WicketTester tester;
+
+    @Before
+    public void setUp() {
+        tester = new WicketTester(new WicketApplication());
+    }
+
+    @Test
+    public void testRenderMyPage() {
+        //start and render the test page
+        tester.startPage(HelloWorld.class);
+
+        //assert rendered page class
+        tester.assertRenderedPage(HelloWorld.class);
+
+        //assert rendered label component
+        tester.assertLabel("message", "If you see this message wicket is properly configured and running");
+    }
+}
